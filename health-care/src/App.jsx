8b6a01f92a8c9b9bb180f5f881fe0f311fa198be
kavitaa30 <kavitavidhate034@gmail.com>
2025-09-12 15:34:23 +0530

    navbar update

--- health-care/src/App.jsx
+++ health-care/src/App.jsx
@@ -1,20 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import Dashboard from './pages/Dashboard.jsx'
-import React from 'react'
-import  LoginPage from './login.jsx'
+import React, { useState } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import "./App.css";
+
+// Pages
+import Dashboard from "./pages/Dashboard.jsx";
+import LoginPage from "./login.jsx";
+import Hospital from "./pages/Hospital.jsx";
+
+// Navbar
+import Navbar from "./components/layout/navbar.jsx";
+
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
-     <div>
-      <Dashboard/>
-      <div>hello</div>
-      <LoginPage/>
-     </div>
-  )
+    <BrowserRouter>
+      {/* Navbar always visible */}
+      <Navbar />
+
+      {/* Page Routes */}
+      <Routes>
+        <Route path="/" element={<Dashboard />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/hospitals" element={<Hospital />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
--- health-care/src/routes/Router.jsx
+++ health-care/src/routes/Router.jsx
@@ -1,8 +1,39 @@
+//import React from "react";
+//import { BrowserRouter, Routes, Route } from "react-router-dom";
+//const Router = () => {
+   
+   
+//}
+
+//export default Router;
+
+
+
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+// pages
+import Dashboard from "../pages/Dashboard.jsx";
+import LoginPage from "../login.jsx";
+import Hospital from "../pages/Hospital.jsx";
+
+// layout
+import Navbar from "../components/layout/navbar.jsx";
+
 const Router = () => {
-   
-   
-}
+  return (
+    <BrowserRouter>
+      {/* Navbar will always show */}
+      <Navbar />
+
+      {/* Routes */}
+      <Routes>
+        <Route path="/" element={<Dashboard />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/hospitals" element={<Hospital />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
 
 export default Router;
